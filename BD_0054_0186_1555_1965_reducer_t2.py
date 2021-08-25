2c9e2a83db2c6868a560ae0d2ea18d58b40f762b
Suhas R <54276824+suhas-rk@users.noreply.github.com>
2021-08-26 00:44:08 +0530

    Update BD_0054_0186_1555_1965_reducer_t2.py

--- BD_0054_0186_1555_1965_reducer_t2.py
+++ BD_0054_0186_1555_1965_reducer_t2.py
@@ -8,22 +8,29 @@ def main():
     rank_unchanged = False
     current_total = 0
     out_list = []
+    
     for inp in sys.stdin:
         line = inp.strip()
         node, rank = line.split("\t")
         if node == current_node:
+            
             if rank[0] != '[':
                 rank = float(rank)
                 current_total += rank
                 current_node_record_count += 1
             else:
                 rank_unchanged = True
+                
         else:
+            
             if current_node != None and rank_unchanged and current_node_record_count == 0:
                 print(current_node, 0.15000, sep=",")
             elif current_node != None:
+                #Print newly calculated page rank according to the formula
                 print(current_node, round((0.15) + (0.85 * current_total), 5), sep=",")
+                
             current_node = node
+            
             if rank[0] != "[":
                 rank = float(rank)
                 current_total = rank
